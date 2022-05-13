333873cc3777c87292c0d4aa339bb01429aad9ac
yuliasigova <yulya12yulya@gmail.com>
2022-05-13 17:41:23 +0400

    Исправила отображение блоков при переполнении

--- build/js/main.js
+++ build/js/main.js
@@ -106,20 +106,28 @@ var menuClickHandler = function menuClickHandler() {
     var button = navMain.querySelector('button');
     button.addEventListener('click', function () {
       navMain.classList.toggle('is-active');
-      document.body.style.overflow = 'hidden';
-      document.body.style.backgroundColor = 'rgba(0, 43, 102, 0.5)';
       header.classList.toggle('is-active');
       navMain.style.overflow = 'auto';
+      document.body.style.overflow = 'hidden';
+      document.body.style.backgroundColor = 'rgba(0, 43, 102, 0.5)';
 
       if (!navMain.classList.contains('is-active')) {
         document.body.style.overflow = 'auto';
-        document.body.style.background = 'none';
+        document.body.style.backgroundColor = '';
       }
     });
   }
 };
 
 menuClickHandler();
+document.body.addEventListener('click', function (evt) {
+  if (evt.target === header) {
+    navMain.classList.remove('is-active');
+    document.body.style.overflow = 'auto';
+    header.classList.remove('is-active');
+    document.body.style.backgroundColor = '';
+  }
+});
 
 /***/ })
 
--- source/js/main.js
+++ source/js/main.js
@@ -10,20 +10,27 @@ if (navMain.classList.contains('no-js')) {
 const menuClickHandler = () => {
   if (navMain) {
     const button = navMain.querySelector('button');
-
     button.addEventListener('click', () => {
       navMain.classList.toggle('is-active');
-      document.body.style.overflow = 'hidden';
-      document.body.style.backgroundColor = 'rgba(0, 43, 102, 0.5)';
       header.classList.toggle('is-active');
       navMain.style.overflow = 'auto';
+      document.body.style.overflow = 'hidden';
+      document.body.style.backgroundColor = 'rgba(0, 43, 102, 0.5)';
       if (!navMain.classList.contains('is-active')) {
         document.body.style.overflow = 'auto';
-        document.body.style.background = 'none';
+        document.body.style.backgroundColor = '';
       }
     });
   }
-
 };
 
 menuClickHandler();
+
+document.body.addEventListener('click', (evt) => {
+  if (evt.target === header) {
+    navMain.classList.remove('is-active');
+    document.body.style.overflow = 'auto';
+    header.classList.remove('is-active');
+    document.body.style.backgroundColor = '';
+  }
+});
